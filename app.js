74067070a7667f259dd50b8d9f4223d87f6234c1
ng1n3 <code.with.muyiwa@gmail.com>
2023-03-24 15:04:13 +0100

    fullpage web done

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+
 const callback = (entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            document.body.style.backgroundColor = entry.target.dataset.color;
-        };
-    });
+
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      document.body.style.backgroundColor = entry.target.dataset.color;
+    }
+  });
 };
 
 const issues = document.querySelectorAll('.issue');
-const observer = new IntersectionObserver(callback, {threshold: .25});
+const issuesRefs = document.querySelectorAll('#issue-ref');
+const observer = new IntersectionObserver(callback, { threshold: 0.25 });
 
 issues.forEach(issue => {
-    observer.observe(issue);
+  observer.observe(issue);
 });
 
-// for(let i = 0; i < issues.length; i++) {
-//     issues[i].style.fontWeight = 700;
-// }
