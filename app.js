e05dbba60f177986bba31e267d79a2d1110d03e2
Ng1n3 <code.with.muyiwa@gmail.com>
2023-04-04 13:22:57 +0000

    anchor color fix

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-
+const selectedStores = document.getElementById('selected-stores')
 const callback = (entries, observer) => {
 
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       document.body.style.backgroundColor = entry.target.dataset.color;
+      // selectedStores.style.color = '#fff';
+      if(document.body.style.backgroundColor !== '#fff') {
+        selectedStores.style.color= '#fff';
+      } else {
+        selectedStores.style.color = '#e568ac';
+      }
     }
   });
 };
